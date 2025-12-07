fd7243403580cd39b0e476de08ca796a9828ba83
SzokolAttila <szokol.ati2005@gmail.com>
2025-12-07 11:59:46 +0100

    added basics of swagger, created gitignore

--- WorkSessionTrackerAPI/Program.cs
+++ WorkSessionTrackerAPI/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); // Required for Swashbuckle to discover endpoints
+builder.Services.AddSwaggerGen(); // Adds Swagger generation services
 
 
 var app = builder.Build();
@@ -10,7 +11,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger(); // Enables the Swagger middleware
+    app.UseSwaggerUI(); // Enables the Swagger UI middleware
+    // You can optionally configure the Swagger UI endpoint, e.g., app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+    // If you were using app.MapOpenApi(), you would remove it here.
 }
 
 app.UseHttpsRedirection();
